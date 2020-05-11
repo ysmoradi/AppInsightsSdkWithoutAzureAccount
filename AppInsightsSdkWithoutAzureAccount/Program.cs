64ed2fee4d6e8d8fb553780ca96d630708e595e2
ysmoradi <ysmoradi@outlook.com>
2020-05-11 10:31:52 +0430

    small enhancements

--- AppInsightsSdkWithoutAzureAccount/Program.cs
+++ AppInsightsSdkWithoutAzureAccount/Program.cs
@@ -1,19 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AppInsightsSdkWithoutAzureAccount
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args)
+                .Build()
+                .RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- AppInsightsSdkWithoutAzureAccount/Startup.cs
+++ AppInsightsSdkWithoutAzureAccount/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,19 +8,21 @@ namespace AppInsightsSdkWithoutAzureAccount
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public Startup(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
+            if (WebHostEnvironment.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseRouting();
 
