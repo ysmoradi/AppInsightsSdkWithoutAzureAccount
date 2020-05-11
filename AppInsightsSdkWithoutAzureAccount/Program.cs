7b040edacf4368eb4ac61453767be6da386fd6fe
ysmoradi <ysmoradi@outlook.com>
2020-05-11 11:08:19 +0430

    added app insights manually.

--- AppInsightsSdkWithoutAzureAccount/Program.cs
+++ AppInsightsSdkWithoutAzureAccount/Program.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AppInsightsSdkWithoutAzureAccount
 {
@@ -15,6 +16,13 @@ namespace AppInsightsSdkWithoutAzureAccount
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logginBuilder =>
+                {
+                    logginBuilder.ClearProviders();
+                    // app insights will add itself to loggingProviders, when you call services.AddApplicationInsightsTelemetry in Startup.cs
+                    // You can clear other providers to reduce pressure.
+                    // You can configure eventFlowConfig.json to submit your logs to multiple outputs such as Elastic.
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- AppInsightsSdkWithoutAzureAccount/Startup.cs
+++ AppInsightsSdkWithoutAzureAccount/Startup.cs
@@ -1,22 +1,30 @@
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace AppInsightsSdkWithoutAzureAccount
 {
     public class Startup
     {
         public IWebHostEnvironment WebHostEnvironment { get; }
+        public IConfiguration Configuration { get; }
 
-        public Startup(IWebHostEnvironment webHostEnvironment)
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
         {
             WebHostEnvironment = webHostEnvironment;
+            Configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry(Configuration);
         }
 
         public void Configure(IApplicationBuilder app)
@@ -30,6 +38,18 @@ namespace AppInsightsSdkWithoutAzureAccount
             {
                 endpoints.MapGet("/", async context =>
                 {
+                    TelemetryClient telemetryClient = context.RequestServices.GetRequiredService<TelemetryClient>(); // You can inject TelemetryClient in controllers and services to provide additional data to app insights sdk.
+
+                    telemetryClient.TrackEvent("MyEvent", new Dictionary<string, string>
+                    {
+                        { "prop1", "1" },
+                        { "prop2", "2" }
+                    });
+
+                    ILogger<ClaimsIdentity> logger = context.RequestServices.GetRequiredService<ILogger<ClaimsIdentity>>(); // 3rd party libraries which relies on MS.Ext.Logging will also be captured by app insights sdk.
+
+                    logger.LogWarning("User {user} was logged in...", "admin");
+
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
