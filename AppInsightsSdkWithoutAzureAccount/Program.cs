0eb6a299354fda9456471c06aeddd88d1ff02c79
ysmoradi <ysmoradi@outlook.com>
2020-05-11 11:13:46 +0430

    added Microsoft.Diagnostics.EventFlow

--- AppInsightsSdkWithoutAzureAccount/Program.cs
+++ AppInsightsSdkWithoutAzureAccount/Program.cs
@@ -1,5 +1,9 @@
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Diagnostics.EventFlow.ApplicationInsights;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -9,23 +13,27 @@ namespace AppInsightsSdkWithoutAzureAccount
     {
         public static async Task Main(string[] args)
         {
-            await CreateHostBuilder(args)
-                .Build()
-                .RunAsync();
+            using (var pipeline = DiagnosticPipelineFactory.CreatePipeline("eventFlowConfig.json"))
+            {
+                await CreateHostBuilder(args, pipeline)
+                    .Build()
+                    .RunAsync();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, DiagnosticPipeline pipeline) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logginBuilder =>
-                {
-                    logginBuilder.ClearProviders();
-                    // app insights will add itself to loggingProviders, when you call services.AddApplicationInsightsTelemetry in Startup.cs
-                    // You can clear other providers to reduce pressure.
-                    // You can configure eventFlowConfig.json to submit your logs to multiple outputs such as Elastic.
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            .ConfigureServices(services => services.AddSingleton<ITelemetryProcessorFactory>(sp => new EventFlowTelemetryProcessorFactory(pipeline)))
+            .ConfigureLogging(logginBuilder =>
+            {
+                logginBuilder.ClearProviders();
+                // app insights will add itself to loggingProviders, when you call services.AddApplicationInsightsTelemetry in Startup.cs
+                // You can clear other providers to reduce pressure.
+                // You can configure eventFlowConfig.json to submit your logs to multiple outputs such as Elastic.
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
